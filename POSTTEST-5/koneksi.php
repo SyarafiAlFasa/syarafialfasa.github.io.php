e70d9a05d6eb532eff9daa48e10cf0b89dd6e6b5
Muhammad Syarafi Al Fasa <113402266+SyarafiAlFasa@users.noreply.github.com>
2022-10-20 21:38:21 +0800

    Delete POSTTEST-5 directory

--- POSTTEST-5/koneksi.php
+++
@@ -1,8 +0,0 @@
-<?php
-$conn = mysqli_connect("localhost","root","","posttest5");
-if (!$conn) {
-    die("Koneksi gagal : " .  mysqli_connect_error());
-} else {
-    echo "Koneksi berhasil";
-}
-?>
